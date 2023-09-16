e4b4d41e3ddea27054cba4b2f4219ef93474814b
PedroH-Neves <PedroHDN710@gmail.com>
2023-09-16 16:07:58 -0400

    Add book class

--- student.rb
+++ student.rb
@@ -6,6 +6,10 @@ class Studentes < Person
     @classroom = classroom
   end
 
+  def classroom_assign(classroom_assign)
+    @classroom = classroom_assign
+    classroom_assign.student.push(self) unless classroom_assign.student.include?(self)
+
   def play_hooky
     '¯\(ツ)/¯'
   end
