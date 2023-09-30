3e3f474df918285323516eaa65de5fdef1561757
PedroH-Neves <PedroHDN710@gmail.com>
2023-09-30 19:42:40 -0400

    Add load json files

--- data/manager.rb
+++ data/manager.rb
@@ -2,7 +2,13 @@ require 'json'
 require 'pry'
 
 class Manager
-  def initialize(data)
+  RENTALS_PATH = './store/rentals.json'.freeze
+  BOOKS_PATH = './store/books.json'.freeze
+  PEOPLES_PATH = './store/peoples.json'.freeze
+
+  attr_accessor :books, :peoples, :rentals
+
+  def initialize(data = nil)
     @data = data
   end
 
@@ -11,73 +17,58 @@ class Manager
   end
 
   def save_file
-   json_data.each do |item|
-    file_path = "./store/#{item.keys.first}.json"
-    File.open(file_path, "w") {|file| file.write(item.values.first)}
-   end
+    json_data.each do |item|
+      file_path = "./store/#{item.keys.first}.json"
+      File.write(file_path, item.values.first)
+    end
+  end
+
+  def self.load_books
+    obj = parsed_file(BOOKS_PATH)
+    deserialize_objects(obj)
+  end
+
+  def self.load_peoples
+    obj = parsed_file(PEOPLES_PATH)
+    deserialize_objects(obj)
+  end
+
+  def self.load_rentals
+    obj = parsed_file(RENTALS_PATH)
+    deserialize_objects(obj)
+  end
+
+  def self.deserialize_objects(obj_list)
+    obj_list.map { |file| Marshal.load(file) }
+  end
+
+  def self.parsed_file(path)
+    json_file = File.read(path)
+    JSON.parse(json_file)
   end
 
   def json_data
     books = create_json_books(@data[:books])
     peoples = create_json_peoples(@data[:peoples])
     rentals = create_json_rentals(@data[:rentals])
-    [{books: books}, {peoples: peoples}, {rentals: rentals}]
+    [{ books: }, { peoples: }, { rentals: }]
   end
 
   private
 
   def create_json_books(books)
-    result = []
-
-    books.each do |book|
-      result <<  
-      {
-        author: book.author,
-        title: book.title
-      }
-    end
-    result.to_json
+    serialize_objects(books).to_json
   end
 
   def create_json_peoples(peoples)
-    result = []
-
-    peoples.each do |people|
-      hash = {
-        id: people.id,
-        age: people.age,
-        name: people.name,
-        type: people.class.to_s,
-        parent_permission: people.parent_permission,
-      }
-      hash["classroom"] = people.classroom if people.is_a?(Student)
-      hash["specialization"] = people.specialization if people.is_a?(Teacher)
-      hash["rentals"] = rentals?(people.rentals) ? [] : rentals(people.rentals)
-
-      result << hash
-    end
-    result.to_json
-  end
-
-  def rentals?(rentals)
-    rentals.empty?
-  end
-
-  def rentals(rentals)
-    result = []
-
-    rentals.each do |rental|
-      result <<
-      {
-        date: rental.date,
-        book: rental.book.title,
-        person: rental.person.name,
-      }
-    end
-    result       
+    serialize_objects(peoples).to_json
   end
 
   def create_json_rentals(rentals)
-    rentals(rentals).to_json
+    serialize_objects(rentals).to_json
+  end
+
+  def serialize_objects(obj_list)
+    obj_list.map { |obj| Marshal.dump(obj) }
   end
 end
