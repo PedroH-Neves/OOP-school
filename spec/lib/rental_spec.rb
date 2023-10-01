8cafba24f98ea22aa215a5fb7f5d62174a134a24
PedroH-Neves <PedroHDN710@gmail.com>
2023-09-30 22:49:51 -0400

    Add rentals test

---
+++ spec/lib/rental_spec.rb
@@ -0,0 +1,18 @@
+require 'rental'
+require 'book'
+require 'person'
+
+describe Rental do 
+  let(:date) { '07-01-2002' }
+  let(:book) { Book.new('Dune', 'Frank Herbert')}
+  let(:person) { Person.new('18', 'John')}
+  let(:instance) {described_class.new(date, book, person)}
+
+  describe '.initialize' do
+    it 'has the right args' do
+      expect(instance.date).to eq('07-01-2002')
+      expect(instance.book).to be_instance_of(Book)
+      expect(instance.person).to be_instance_of(Person)
+    end
+  end
+end
