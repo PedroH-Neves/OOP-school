9c3545825b71a5478ce32ca7b8924fb8cee89af5
PedroH-Neves <PedroHDN710@gmail.com>
2023-09-30 22:38:56 -0400

    Add classroom tests

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,5 +1,5 @@
 class Classroom
-  attr_accessor :label, :students
+  attr_accessor :label, :students, :classroom
 
   def initialize(label)
     @label = label
---
+++ spec/lib/classroom_spec.rb
@@ -0,0 +1,13 @@
+require 'classroom'
+
+describe Classroom do
+  let(:label) { 'Microverse' }
+  let(:instance){ described_class.new(label) }
+
+  describe '.initialize' do
+    it 'has the right args' do
+      expect(instance.label).to eq('Microverse')
+      expect(instance.classroom).to be_empty
+    end
+  end
+end
