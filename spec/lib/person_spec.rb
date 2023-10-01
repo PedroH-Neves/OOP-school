bf00bc84559d04cd2e20e2762c2b8e03433dd96f
PedroH-Neves <PedroHDN710@gmail.com>
2023-09-30 21:24:29 -0400

    Add person test

---
+++ spec/lib/person_spec.rb
@@ -0,0 +1,60 @@
+require 'person'
+require 'rental'
+
+describe Person do 
+  let(:age) { '12' }
+  let(:name) { 'John Doe' }
+  let(:instance) {described_class.new(age, name)}
+  
+  describe '#add_rental' do
+    let(:book) { 'frankstein' }
+    let(:date) { '10' }
+    let(:rental) { instance_double(Rental) }
+
+    before do
+      allow(Rental).to receive(:new).and_return(rental)
+    end
+
+    subject { instance.add_rental(book, date) }
+
+    it 'pushes rental class to instance rental array' do
+      subject
+
+      expect(instance.rentals.empty?).to be(false)
+    end
+
+    it 'create new instance of Rental class' do
+      expect(Rental).to receive(:new).with(book, date, instance.class)
+      
+      subject
+    end
+  end
+
+  describe '#correct_name' do
+    subject { instance.correct_name }
+
+    it 'it returns the atribute name' do
+      expect(subject).to eq('John Doe')
+    end
+  end
+
+  describe '#of_age?' do
+    context 'above age' do
+      let(:age) { '21' }
+      let(:instance) {described_class.new(age, name)}
+
+      subject { instance.of_age? }
+
+      it 'returns true' do
+        expect(subject).to eq(true) 
+      end
+    end
+    context 'below age' do
+      subject { instance.of_age? }
+
+      it 'returns false' do
+        expect(subject).to eq(false)
+      end
+    end    
+  end
+end
